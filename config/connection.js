77066c68ca48e0061c33f44d4433fec8a311ef9d
kurt.iamele <kurt.iamele@gmail.com>
2020-07-14 20:19:39 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Shsk0707",
+    password: "password",
     database: "workoutdb"
 });
 
