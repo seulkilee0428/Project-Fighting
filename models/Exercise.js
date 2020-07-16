c552d32b99132d4025f8535d577b575e2b9aaa92
kurt.iamele <kurt.iamele@gmail.com>
2020-07-15 23:46:46 -0500

    updates

--- models/Exercise.js
+++ models/Exercise.js
@@ -7,11 +7,32 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1]
             }
         },
-        body: {
+        desc: {
             type: DataTypes.TEXT,
             allowNull: false,
             len: [1]
+        },
+        steps: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
+        },
+        tips: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
+        },
+        warn: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
+        },
+        videoUrl: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            len: [1]
         }
+
     });
 
     Exercise.associate = function (models) {
